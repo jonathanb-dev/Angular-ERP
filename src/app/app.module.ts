fb4f56a54b85f011f03c6e220abc0a6a62cb26be
Jonathan Bernard <jonathansemal@gmail.com>
2020-07-27 20:36:16 +0200

    Add base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
+// Pages
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+// Components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { LayoutComponent } from './layouts/layout/layout.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NavbarComponent } from './layouts/header/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotFoundPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LayoutComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
